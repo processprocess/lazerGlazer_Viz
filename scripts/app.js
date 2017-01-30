569fa61290b97db8bd50c8fcd51b148316cbdd29
thoragio <thoragio@gmail.com>
2017-01-30 10:21:57 +0100

    Refactor JS code, add getRandomShape function

--- scripts/app.js
+++ scripts/app.js
@@ -1,111 +1,70 @@
-//GRAB SCENE
-let scene = document.querySelector(`a-scene`);
+const scene = document.querySelector('a-scene')
+const sky = document.querySelector('a-sky')
+const objectContainer = document.querySelector('#object-container')
 
-//SET CONTAINER
-let objectContainer = document.createElement(`a-entity`);
-objectContainer.setAttribute(`position`, `0 1.6 -2`);
-objectContainer.setAttribute(`scale`, `.4 .4 .4`);
-scene.appendChild(objectContainer);
-
-//RANDOM COLOR FUNCTION
-function getRandomColor(){
-  let randomColor = Math.floor(Math.random()*(16777215-600000+1)+600000).toString(16);
-  // let randomColor = Math.floor(Math.random()*16777215).toString(16);
-  return randomColor;
+function getRandomNumber(x, y) {
+  return Math.floor(Math.random() * x + y)
 }
 
-//RANDOM NUMBER FUNCTION
-function getRandomnumber(){
-  let randomNumber = Math.floor(Math.random()*(30-10+1)+10);
-  return randomNumber;
-}
+const totalSteps = getRandomNumber(17, 10)
+const totalRotations = getRandomNumber(17, 10)
 
-//SET AMOUNT OF ELEMENTS
-let totalSteps = getRandomnumber();
-let totalRotations = getRandomnumber();
-
-//SET LIGHTS
-let sceneSky = document.createElement(`a-sky`);
-sceneSky.setAttribute(`color`, `#${getRandomColor()}`);
-sceneSky.setAttribute(`animation__color`, `property:color ; dir:alternate ; dur:2000 ; easing:easeInOutSine ; loop:true ; to:#${getRandomColor()}`);
-scene.appendChild(sceneSky);
-
-let ambientLight = document.createElement(`a-light`);
-ambientLight.setAttribute(`id`, `theAmbientLight`);
-ambientLight.setAttribute(`type`, `ambient`);
-ambientLight.setAttribute(`intensity`, `.7`);
-ambientLight.setAttribute(`color`, `#FFFFFF`);
-scene.appendChild(ambientLight);
-
-let light1 = document.createElement(`a-light`);
-light1.setAttribute(`id`, `blueToRedLight`);
-light1.setAttribute(`color`, `#0000FF`);
-light1.setAttribute(`intensity`, `3`);
-light1.setAttribute(`position`, `-5.72 6.65 0.80`);
-light1.setAttribute(`animation__color`, `property:color ; dir:alternate ; dur:2000 ; easing:easeInOutSine ; loop:true ; to:#FF0000`);
-light1.setAttribute(`alongpath`, `path:10,10,-10 -20,10,-10 10,0,-10; closed:true; dur:12000;`);
-scene.appendChild(light1);
-
-let light2 = document.createElement(`a-light`);
-light2.setAttribute(`id`, `blueToRedLight`);
-light2.setAttribute(`color`, `#FF0000`);
-light2.setAttribute(`intensity`, `5`);
-light2.setAttribute(`position`, `8.60 6.65 0.80`);
-light2.setAttribute(`animation__color`, `property:color ; dir:alternate ; dur:2000 ; easing:easeInOutSine ; loop:true ; to:#0000FF`);
-light2.setAttribute(`alongpath`, `path:-2,-2,5 2,-1,5 0,-1,5; closed:true; dur:3000;`);
-scene.appendChild(light2);
-
-//GENERATE ELEMENTS AND APPEND THEM ONTO ROTATION CONTAINERS
-for (let i=1 ; i<=totalRotations ; i++){
-  let currentRotation = 360 / totalRotations * i;
-  let rotateContainer = document.createElement(`a-entity`);
-  rotateContainer.setAttribute(`rotation`, `0 0 ${currentRotation}`);
-
-  for (let i=1 ; i<=totalSteps ;i++) {
-    let circleElementContainer = document.createElement(`a-entity`);
-    circleElementContainer.setAttribute(`class`, `circleElementContainer${i}`);
-    let evenDistance = i / totalSteps  ;
-    // let evenDistance = i * 1.5 / totalSteps * i ; //save this for true evenness for elements
-    circleElementContainer.setAttribute(`position`, `0 ${evenDistance} 0`);
-    let randomColorForThis = getRandomColor();
-    let circleElement = document.createElement(`a-entity`);
-    circleElement.setAttribute(`class`, `circleElement${i}`);
-    let currentSize = i / totalSteps;
-    circleElement.setAttribute(`scale`, `${currentSize} ${currentSize} ${currentSize}`);
-    circleElement.setAttribute(`material`, `color:#${randomColorForThis} ; metalness:0 ; roughness:0; `);
-    circleElement.setAttribute(`geometry`, `primitive:sphere; radius:1.5;`);
-    circleElement.setAttribute(`animation__yoyo`, `property: scale; dir: alternate; dur: ${currentSize*10000}; easing: easeInOutSine; loop: true; to: 0 0 0`);
-    circleElementContainer.appendChild(circleElement);
-    rotateContainer.appendChild(circleElementContainer);
+function getRandomColor() {
+  const letters = '0123456789abcdef'
+  let randomColor = ''
+  for (let i = 0; i < 6; i++) {
+    randomColor += letters[Math.floor(Math.random() * 16)]
   }
-  objectContainer.appendChild(rotateContainer);
-}
-//GET VALUE FOR PATH RANDOMIZATION
-function pathValue(){
-  return Math.floor(Math.random()*(10*2+1)-10);
-}
-//GET DURATION RANDOMIZATION FOR PATH
-function duration(){
-  return Math.floor(Math.random()*(10-5+1)+5);
+  return randomColor
 }
 
-//GRAB EVERY OTHER SPHERE ELEMENT IN RING AND ALTER ITS PATH
-for(let i=0 ; i<=totalSteps ; i++){
-  let circleRing = document.getElementsByClassName(`circleElement${i}`);
+sky.setAttribute('color', `#${getRandomColor()}`)
+sky.setAttribute('animation__color', `property: color; dir: alternate; dur: 2000; easing: easeInOutSine; loop: true; to: #${getRandomColor()}`)
 
-      let valueOne = pathValue()
-      let valueTwo = pathValue()
-      let randomDuration = duration()
+function getRandomShape() {
+  const shapes = ['sphere', 'octahedron', 'icosahedron', 'torus', 'tetrahedron']
+  return shapes[Math.floor(Math.random() * shapes.length)]
+}
 
-    for (let i=0 ; i<circleRing.length ; i++){
-      if(i%2 == 0){
-      circleRing[i].setAttribute(`alongpath`, `path: 0,0,0 ${valueOne},${valueTwo},${valueOne} ${valueTwo},${valueOne},${valueTwo} ; closed:true ; dur:${randomDuration}000 ; loop:true`);
-    }else{
-      circleRing[i].setAttribute(`alongpath`, `path: 0,0,0 ${valueTwo},${valueOne},${valueTwo} ${valueOne},${valueTwo},${valueOne} ; closed:true ; dur:${randomDuration}000 ; loop:true`);
+function generateElements() {
+  for (let i = 1; i <= totalRotations; i++) {
+    const currentRotation = 360 / totalRotations * i
+    const rotateContainer = document.createElement('a-entity')
+    rotateContainer.setAttribute('rotation', `0 0 ${currentRotation}`)
+    for (let j = 1; j <= totalSteps; j++) {
+      const evenDistance = j / totalSteps
+      const currentSize = j / totalSteps
+      const circleElementContainer = document.createElement('a-entity')
+      circleElementContainer.setAttribute('class', `circleElementContainer${j}`)
+      circleElementContainer.setAttribute('position', `0 ${evenDistance} 0`)
+      const circleElement = document.createElement('a-entity')
+      circleElement.setAttribute('class', `circleElement${j}`)
+      circleElement.setAttribute('scale', `${currentSize} ${currentSize} ${currentSize}`)
+      circleElement.setAttribute('material', `color:#${getRandomColor()}; metalness: 0; roughness: 0`)
+      circleElement.setAttribute('geometry', `primitive: ${getRandomShape()}; radius: 1.5`)
+      circleElement.setAttribute('animation__yoyo', `property: scale; dir: alternate; dur: ${currentSize * 10000}; easing: easeInOutSine; loop: true; to: 0 0 0`)
+      circleElementContainer.appendChild(circleElement)
+      rotateContainer.appendChild(circleElementContainer)
+    }
+    objectContainer.appendChild(rotateContainer)
+  }
+}
+
+function alterEveryOtherPath() {
+  for (let i = 0; i <= totalSteps; i++) {
+    let circleRing = document.getElementsByClassName(`circleElement${i}`)
+    let valueOne = getRandomNumber(21, -10)
+    let valueTwo = getRandomNumber(21, -10)
+    let randomDuration = getRandomNumber(6, 5)
+    for (let j = 0; j < circleRing.length; j++) {
+      if (j % 2 == 0) {
+        circleRing[j].setAttribute('alongpath', `path: 0,0,0 ${valueOne},${valueTwo},${valueOne} ${valueTwo},${valueOne},${valueTwo}; closed: true; dur: ${randomDuration}000; loop: true`)
+      } else {
+        circleRing[j].setAttribute('alongpath', `path: 0,0,0 ${valueTwo},${valueOne},${valueTwo} ${valueOne},${valueTwo},${valueOne}; closed: true; dur: ${randomDuration}000; loop: true`)
+      }
     }
   }
 }
 
-// TODO: fix getRandomColor to generate only valid hex values
-// TODO: DRY out randomization functions. Feeling like I have too many
-// TODO: encapsulate logic to minimize global variables
+generateElements()
+alterEveryOtherPath()
